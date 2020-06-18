e1b691a23c9cc38ef188295607332a91f8463af4
Olamide james <olamidejames007@gmail.com>
2020-06-18 17:07:28 +0100

    initial commit

---
+++ NetCorePushSample/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NetCorePushSample.Services;
+using NetCorePushSample.Views;
+
+namespace NetCorePushSample
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
